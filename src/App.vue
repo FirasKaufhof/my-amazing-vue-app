26513fd2fc91b212d56685f2d88477e69d933ef0
Firas <mohamed.firas.ouertani@kaufhof.de>
2020-03-04 11:27:17 +0100

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,35 @@
 <template>
   <div id="app">
+    <div class="welcome">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Amazing Vue App"/>
+    </div>
+    <div class="pattern-library-content">
+    <MyButton :isDisabled="false" label="Hello 1st npm package"></MyButton>
+
+    <List tag="ol" type="A">
+        <ListItem tag="li" text="I am the first item.">
+        </ListItem>
+        <ListItem tag="li" text="I am the second item.">
+        </ListItem>
+        <ListItem tag="li" text="I am the third item.">
+        </ListItem>
+    </List>
+    <Badge status="warning"></Badge>
+    <CustomerCard />
+    </div>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Components from '@galeria-demo/my-amazing-storybook-app'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    ...Components
   }
 }
 </script>
@@ -21,8 +39,12 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  text-align: center;
+}
+
+.pattern-library-content {
+list-style: inside;
 }
 </style>
